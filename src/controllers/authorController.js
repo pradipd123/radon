0eda1cba16007f06b28186cdaf8feddc517504a3
pradipd123 <dahikarpradip2@gmail.com>
2022-06-12 16:06:36 +0530

    modified file

---
+++ src/controllers/authorController.js
@@ -0,0 +1,16 @@
+const { count } = require("console")
+const   authorModel= require("../models/authorModel")
+
+const createAuthor= async function (req, res) {
+    let data= req.body
+    let authorId = data.author_id
+    if(!authorId) return res.send({msg:' AuthorId is  mandatory in the request'})
+
+    let savedData= await authorModel.create(data)
+    res.send({data: savedData})
+}
+
+ 
+module.exports.createAuthor=createAuthor 
+ 
+
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Functionup-radon-pradip:si19LZAXAODDsiJu@cluster0.hbt01c8.mongodb.net/Pradip1432-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
